6e3eddc6324e592a03a6361443bb4dbcbb57a243
lidgren <lidgren@16fcafec-0654-0410-b677-e1d0b5eb3b9b>
2008-08-17 10:53:15 +0000

    DurableServer/Client sample added, ChatClient/Server sample removed (temporarily)

---
+++ Samples/DurableServer/Program.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Lidgren.Network;
+using System.Threading;
+
+namespace DurableServer
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			NetConfiguration config = new NetConfiguration("durable");
+			config.MaxConnections = 128;
+			config.Port = 14242;
+			NetServer server = new NetServer(config);
+			server.Start();
+
+			NetBuffer buffer = server.CreateBuffer();
+
+			Console.WriteLine("Press any key to quit");
+			while (!Console.KeyAvailable)
+			{
+				NetMessageType type;
+				NetConnection sender;
+				if (server.ReadMessage(buffer, out type, out sender))
+				{
+					switch (type)
+					{
+						case NetMessageType.StatusChanged:
+							Console.WriteLine("New status: " + sender.Status + " (" + buffer.ReadString() + ")");
+							break;
+						case NetMessageType.BadMessageReceived:
+						case NetMessageType.ConnectionRejected:
+						case NetMessageType.DebugMessage:
+						case NetMessageType.Data:
+							Console.WriteLine(buffer.ReadString());
+							break;
+						default:
+							Console.WriteLine("Unhandled: " + type + " " + buffer.ToString());
+							break;
+					}
+				}
+				Thread.Sleep(5);
+			}
+
+			server.Shutdown("Application exiting");
+		}
+	}
+}
