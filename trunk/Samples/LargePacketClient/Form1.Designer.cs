4ae57ac8034509ac2558d0a0643f81c3cf76cb22
lidgren <lidgren@16fcafec-0654-0410-b677-e1d0b5eb3b9b>
2009-03-30 08:43:49 +0000

    Disconnect button added to LargePacket sample

--- trunk/Samples/LargePacketClient/Form1.Designer.cs
+++ trunk/Samples/LargePacketClient/Form1.Designer.cs
@@ -31,6 +31,7 @@ namespace LargePacketClient
 			this.button1 = new System.Windows.Forms.Button();
 			this.richTextBox1 = new System.Windows.Forms.RichTextBox();
 			this.label1 = new System.Windows.Forms.Label();
+			this.button2 = new System.Windows.Forms.Button();
 			this.SuspendLayout();
 			// 
 			// button1
@@ -64,11 +65,22 @@ namespace LargePacketClient
 			this.label1.TabIndex = 2;
 			this.label1.Text = "label1";
 			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(146, 12);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(128, 29);
+			this.button2.TabIndex = 3;
+			this.button2.Text = "Disconnect";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(562, 500);
+			this.Controls.Add(this.button2);
 			this.Controls.Add(this.label1);
 			this.Controls.Add(this.richTextBox1);
 			this.Controls.Add(this.button1);
@@ -85,6 +97,7 @@ namespace LargePacketClient
 		private System.Windows.Forms.Button button1;
 		public System.Windows.Forms.RichTextBox richTextBox1;
 		public System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Button button2;
 	}
 }
 
