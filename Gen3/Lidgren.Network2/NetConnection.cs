55f255a9c6921a01611b31fa11dfd9862aa00696
lidgren <lidgren@16fcafec-0654-0410-b677-e1d0b5eb3b9b>
2009-11-11 22:25:49 +0000

    experimental code added in Gen3 folder

---
+++ Gen3/Lidgren.Network2/NetConnection.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+
+namespace Lidgren.Network2
+{
+	public class NetConnection
+	{
+		private IPEndPoint m_remoteEndPoint;
+
+		internal NetConnection(IPEndPoint remoteEndPoint)
+		{
+			m_remoteEndPoint = remoteEndPoint;
+		}
+
+		internal void Heartbeat()
+		{
+		}
+	}
+}
