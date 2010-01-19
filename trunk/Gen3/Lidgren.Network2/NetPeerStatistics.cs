7660a16e05f49a9d2317bc96c616cd51853a7f21
lidgren <lidgren@16fcafec-0654-0410-b677-e1d0b5eb3b9b>
2010-01-19 07:32:48 +0000

    more gen3

---
+++ trunk/Gen3/Lidgren.Network2/NetPeerStatistics.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lidgren.Network2
+{
+	public sealed class NetPeerStatistics
+	{
+		internal int m_sentPackets;
+		internal int m_receivedPackets;
+
+		internal NetPeerStatistics()
+		{
+			Reset();
+		}
+
+		internal void Reset()
+		{
+			m_sentPackets = 0;
+			m_receivedPackets = 0;
+		}
+
+		/// <summary>
+		/// Gets the number of sent packets since the NetPeer was initialized
+		/// </summary>
+		public int SentPackets { get { return m_sentPackets; } }
+
+		/// <summary>
+		/// Gets the number of received packets since the NetPeer was initialized
+		/// </summary>
+		public int ReceivedPackets { get { return m_receivedPackets; } }
+	}
+}
